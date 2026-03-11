74fc9b48f8f5a7239e48395efb7d579058801ccf
Demian Catalán <demiancrucesc@gmail.com>
2026-03-10 23:43:38 -0300

    Update Analizador.java
    Exportación de historial médico a CSV

--- src/proyectoProtesis/Analizador.java
+++ src/proyectoProtesis/Analizador.java
@@ -1,4 +1,7 @@
 package proyectoProtesis;
+import java.io.FileWriter;
+import java.io.PrintWriter;
+
 
 public class Analizador {
 	public void evaluarMarcha(double[] datosDelSensor) {
@@ -12,10 +15,32 @@ public class Analizador {
         	}
         	i++ ;
         	
+        	
         }
         
         System.out.println("Análisis de marcha terminado. Pasos críticos (>20G): "+ pasosCriticos) ;
         
+        guardarHistorialCsv(pasosCriticos);
+	}
+	
+	public void guardarHistorialCsv(int cantPasosCriticos) {
+		try {
+		    
+		    FileWriter archivo = new FileWriter("historial_protesis.csv", true);
+		    PrintWriter escritor = new PrintWriter(archivo);
+
+		    //Texto de prueba CAMBIAR
+		    escritor.println("Pasos Críticos; " + cantPasosCriticos);
+
+		    // Siempre hay que cerrar el archivo para que los cambios se guarden de verdad
+		    escritor.close();
+		    
+		    System.out.println("Registro guardado en Excel (CSV).");
+
+		} catch (Exception e) {
+		    System.out.println("Ocurrió un error al guardar: " + e.getMessage());
+		}
+		
 		
 	}
 
